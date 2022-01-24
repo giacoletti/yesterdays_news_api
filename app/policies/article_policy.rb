be69f020e64c47f96c84308499d26183321ae816
giacoletti <giacoletti92@gmail.com>
2022-01-24 15:32:41 +0100

    Installs pundit-matchers adds pundit specs for article policy updates article policy for index and show actions

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,6 +1,16 @@
 class ArticlePolicy < ApplicationPolicy
   
   def create?
-    @user.is_staff?
+    # @user && @user.is_staff?
+    # @user.try(:is_staff?)
+    @user&.is_staff?
+  end
+
+  def index?
+    true
+  end
+  
+  def show?
+    true
   end
 end
